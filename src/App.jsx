c95a37edd5170672e4827229774e0f433b5d4efa
Maja Gedda <maja.gedda@zocom.se>
2023-04-18 11:05:51 +0200

    useage of dispatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { useDispatch } from 'react-redux'
 import './App.css'
 import CarsContainer from './Components/CarsContainer'
+import { buyCar, addCar } from './actions/carActions'
 
 function App() {
+  const dispatch = useDispatch();
+
+  function handleBuy() {
+    // här vill vi kalla på vår action som motsvarar det vi vill göra
+    dispatch(buyCar('volvo', 1))
+  }
+  function handleAdd() {
+    // här vill vi kalla på vår action som motsvarar det vi vill göra
+    dispatch(addCar('volvo', 3))
+  }
 
   return (
     <div className="App">
       <CarsContainer />
+      <button onClick={handleBuy}>BUY</button>
+      <button onClick={handleAdd}>ADD</button>
     </div>
   )
 }
